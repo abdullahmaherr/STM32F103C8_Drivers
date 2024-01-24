480577e8230aee461cc8487f5d3a3ec5fab4c5f6
Abdullah Maher <abdullahhegazi10@gmail.com>
2024-01-24 20:05:41 +0200

    Update

--- STM32F103C8/Drivers/MCAL/USART/stm32f103c8_usart_driver.c
+++ STM32F103C8/Drivers/MCAL/USART/stm32f103c8_usart_driver.c
@@ -27,22 +27,19 @@
 #define BAUDRATEDIV(__PCLK__, __BAUDRATE__)				((uint32_t) ( (( 25 * __PCLK__ ) / (4 * __BAUDRATE__ ))) )
 #define BRR_REG(__PCLK__,__BAUDRATE__)					( (DIV_Mantissa(BAUDRATEDIV(__PCLK__, __BAUDRATE__)) << 4) | (DIV_Fraction(BAUDRATEDIV(__PCLK__, __BAUDRATE__)) & 0xF) )
 
+
 /*===============================================================================
  *                              Global Variables                                 *
  ================================================================================*/
 /* Array of 3 Global Configuration Structure */
-USART_Config_t g_USART_Config[3];
-
-/* Array of 3 Pointer To Function */
-void (* gp_USART_ISR_CallBack[3])(void);
-
+USART_Config_t* gp_USART_Config[3] = {NULL,NULL,NULL};
 
 /*===============================================================================
  *                          Private Function Prototypes	   		                 *
  ================================================================================*/
 
 /**===============================================================================
- * Function Name  : MCAL_USART_ReceiveData.
+ * Function Name  : MCAL_USART_GPIO_PinConfig.
  * Brief          : Function To Initiate a USARTx GPIO Pins.
  * Parameter (in) : Instant USARTx Could be 1, 2, and 3.
  * Return         : None.
@@ -63,12 +60,12 @@ static void MCAL_USART_GPIO_PinConfig(USART_TypeDef* USARTx);
  * Note           : None.																					*/
 void MCAL_USART_Init(USART_TypeDef* USARTx,USART_Config_t* p_USART_Config)
 {
-	uint32_t PCLK = 0, BRR = 0;
+	uint32_t PCLK = 0, Temp_BRR = 0, Temp_CR1 = 0, Temp_CR2 = 0, Temp_CR3 = 0;
 
 	if(USART1 == USARTx)
 	{
 		/* Assign Global Configuration Structure */
-		g_USART_Config[0] = *p_USART_Config;
+		gp_USART_Config[0] = p_USART_Config;
 
 		/* Enable Clock For The Peripheral */
 		MCAL_RCC_enableCLK(RCC_APB2_BUS, RCC_USART1_ID);
@@ -79,7 +76,7 @@ void MCAL_USART_Init(USART_TypeDef* USARTx,USART_Config_t* p_USART_Config)
 	else if(USART2 == USARTx)
 	{
 		/* Assign Global Configuration Structure */
-		g_USART_Config[1] = *p_USART_Config;
+		gp_USART_Config[1] = p_USART_Config;
 
 		/* Enable Clock For The Peripheral */
 		MCAL_RCC_enableCLK(RCC_APB1_BUS, RCC_USART2_ID);
@@ -90,7 +87,7 @@ void MCAL_USART_Init(USART_TypeDef* USARTx,USART_Config_t* p_USART_Config)
 	else if(USART3 == USARTx)
 	{
 		/* Assign Global Configuration Structure */
-		g_USART_Config[2] = *p_USART_Config;
+		gp_USART_Config[2] = p_USART_Config;
 
 		/* Enable Clock For The Peripheral */
 		MCAL_RCC_enableCLK(RCC_APB1_BUS, RCC_USART3_ID);
@@ -99,58 +96,56 @@ void MCAL_USART_Init(USART_TypeDef* USARTx,USART_Config_t* p_USART_Config)
 		PCLK = MCAL_RCC_GetPCLK1();
 	}
 
+	/* Enable The USART */
+	SET_BIT(Temp_CR1,13);
+
 	/* Enable USART TX/RX */
-	(USARTx->CR1) |= (p_USART_Config->USART_Mode);
+	Temp_CR1 |= (p_USART_Config->USART_Mode);
 
 	/* Configure The USART Word Length */
-	(USARTx->CR1) |= (p_USART_Config->USART_WordLength);
+	Temp_CR1 |= (p_USART_Config->USART_WordLength);
 
 	/* Configure The USART Parity Bit */
-	(USARTx->CR1) |= (p_USART_Config->USART_ParityBit);
+	Temp_CR1 |= (p_USART_Config->USART_ParityBit);
 
 	/* Configure The USART Stop Bits */
-	(USARTx->CR2) |= (p_USART_Config->USART_StopBits);
+	Temp_CR2 |= (p_USART_Config->USART_StopBits);
 
 	/* Configure The USART Flow Control */
-	(USARTx->CR3) |= (p_USART_Config->USART_FlowControl);
+	Temp_CR3 |= (p_USART_Config->USART_FlowControl);
 
 	/* Configure The USART Baud Rate */
-	BRR = BRR_REG(PCLK,p_USART_Config->USART_BaudRate);
-	USARTx->BRR = BRR;
+	Temp_BRR = BRR_REG(PCLK,p_USART_Config->USART_BaudRate);
+	(USARTx->BRR) = Temp_BRR;
 
 	if(USART_IRQ_DISABLE != p_USART_Config->USART_IRQ)
 	{
 		if(USART1 == USARTx)
 		{
-			/* Assign CallBack Function With ISR */
-			gp_USART_ISR_CallBack[0] = p_USART_Config->p_USART_ISR;
 			/* NVIC IRQ Enable  */
 			NVIC_USART1_EN();
 		}
 		else if(USART2 == USARTx)
 		{
-			/* Assign CallBack Function With ISR */
-			gp_USART_ISR_CallBack[1] = p_USART_Config->p_USART_ISR;
 			/* NVIC IRQ Enable  */
 			NVIC_USART2_EN();
 		}
 		else if(USART3 == USARTx)
 		{
-			/* Assign CallBack Function With ISR */
-			gp_USART_ISR_CallBack[2] = p_USART_Config->p_USART_ISR;
 			/* NVIC IRQ Enable  */
 			NVIC_USART3_EN();
 		}
 		/* IRQ MASK Enable */
-		(USARTx->CR1) |= (p_USART_Config->USART_IRQ);
+		Temp_CR1 |= (p_USART_Config->USART_IRQ);
 	}
 
 	/* Configure The GPIO Pins */
 	MCAL_USART_GPIO_PinConfig(USARTx);
 
-	/* Enable The USART */
-	SET_BIT((USARTx->CR1),13);
-
+	/* Assign Control Registers */
+	(USARTx->CR3) = Temp_CR3;
+	(USARTx->CR2) = Temp_CR2;
+	(USARTx->CR1) = Temp_CR1;
 }
 
 /**===============================================================================
@@ -166,7 +161,7 @@ void MCAL_USART_Deinit(USART_TypeDef* USARTx)
 		/* Disable Clock For The Peripheral */
 		MCAL_RCC_disableCLK(RCC_APB2_BUS, RCC_USART1_ID);
 
-		/* NVIC IRQ Enable  */
+		/* NVIC IRQ Disable  */
 		NVIC_USART1_DI();
 	}
 	else if(USART2 == USARTx)
@@ -174,15 +169,15 @@ void MCAL_USART_Deinit(USART_TypeDef* USARTx)
 		/* Disable Clock For The Peripheral */
 		MCAL_RCC_disableCLK(RCC_APB1_BUS, RCC_USART2_ID);
 
-		/* NVIC IRQ Enable  */
+		/* NVIC IRQ Disable  */
 		NVIC_USART2_DI();
 	}
 	else if(USART3 == USARTx)
 	{
-		/* Enable Clock For The Peripheral */
+		/* Disable Clock For The Peripheral */
 		MCAL_RCC_disableCLK(RCC_APB1_BUS, RCC_USART3_ID);
 
-		/* NVIC IRQ Enable  */
+		/* NVIC IRQ Disable  */
 		NVIC_USART3_DI();
 	}
 }
@@ -195,33 +190,33 @@ void MCAL_USART_Deinit(USART_TypeDef* USARTx)
  * Parameter (in) : Polling Mechanism State Enable/Disable.
  * Return         : None.
  * Note           : None.																					*/
-void MCAL_USART_TransmitData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Mechanism a_PollingEn)
+void MCAL_USART_TransmitData(USART_TypeDef* USARTx, uint16_t* p_Buff, USART_Polling_Mechanism a_PollingEn)
 {
-	if(POLLING_ENABLED == a_PollingEn)
+	if(USART_POLLING_ENABLED == a_PollingEn)
 		while(BIT_IS_CLEAR(USARTx->SR,7));
 
 	if(USART1 == USARTx)
 	{
 		/* Assign Data Register */
-		if(USART_Word_Length_8BITS == g_USART_Config[0].USART_WordLength)
+		if(USART_Word_Length_8BITS == gp_USART_Config[0]->USART_WordLength)
 			USARTx->DR = ( *p_Buff & (uint8_t)0xFF );
-		else if(USART_Word_Length_9BITS == g_USART_Config[0].USART_WordLength)
+		else if(USART_Word_Length_9BITS == gp_USART_Config[0]->USART_WordLength)
 			USARTx->DR = ( *p_Buff & (uint16_t)0x1FF );
 	}
 	else if(USART2 == USARTx)
 	{
 		/* Assign Data Register */
-		if(USART_Word_Length_8BITS == g_USART_Config[1].USART_WordLength)
+		if(USART_Word_Length_8BITS == gp_USART_Config[1]->USART_WordLength)
 			USARTx->DR = ( *p_Buff & (uint8_t)0xFF );
-		else if(USART_Word_Length_9BITS == g_USART_Config[1].USART_WordLength)
+		else if(USART_Word_Length_9BITS == gp_USART_Config[1]->USART_WordLength)
 			USARTx->DR = ( *p_Buff & (uint16_t)0x1FF );
 	}
 	else if(USART3 == USARTx)
 	{
 		/* Assign Data Register */
-		if(USART_Word_Length_8BITS == g_USART_Config[2].USART_WordLength)
+		if(USART_Word_Length_8BITS == gp_USART_Config[2]->USART_WordLength)
 			USARTx->DR = ( *p_Buff & (uint8_t)0xFF );
-		else if(USART_Word_Length_9BITS == g_USART_Config[2].USART_WordLength)
+		else if(USART_Word_Length_9BITS == gp_USART_Config[2]->USART_WordLength)
 			USARTx->DR = ( *p_Buff & (uint16_t)0x1FF );
 	}
 }
@@ -234,24 +229,24 @@ void MCAL_USART_TransmitData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Me
  * Parameter (in) : Polling Mechanism State Enable/Disable.
  * Return         : None.
  * Note           : None.																					*/
-void MCAL_USART_ReceiveData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Mechanism a_PollingEn)
+void MCAL_USART_ReceiveData(USART_TypeDef* USARTx, uint16_t* p_Buff, USART_Polling_Mechanism a_PollingEn)
 {
-	if(POLLING_ENABLED == a_PollingEn)
+	if(USART_POLLING_ENABLED == a_PollingEn)
 		while(BIT_IS_CLEAR(USARTx->SR,5));
 
 	if(USART1 == USARTx)
 	{
 		/* Read Data Register */
-		if(USART_Word_Length_8BITS == g_USART_Config[0].USART_WordLength)
+		if(USART_Word_Length_8BITS == gp_USART_Config[0]->USART_WordLength)
 		{
-			if(USART_PARITY_BIT_NONE == g_USART_Config[0].USART_ParityBit)
+			if(USART_PARITY_BIT_NONE == gp_USART_Config[0]->USART_ParityBit)
 				*p_Buff = ( USARTx->DR & (uint8_t)0xFF );
 			else
 				*p_Buff = ( USARTx->DR & (uint8_t)0x7F );
 		}
-		else if(USART_Word_Length_9BITS == g_USART_Config[0].USART_WordLength)
+		else if(USART_Word_Length_9BITS == gp_USART_Config[0]->USART_WordLength)
 		{
-			if(USART_PARITY_BIT_NONE == g_USART_Config[0].USART_ParityBit)
+			if(USART_PARITY_BIT_NONE == gp_USART_Config[0]->USART_ParityBit)
 				*p_Buff = USARTx->DR;
 			else
 				*p_Buff = ( USARTx->DR & (uint8_t)0xFF );
@@ -260,16 +255,16 @@ void MCAL_USART_ReceiveData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Mec
 	else if(USART2 == USARTx)
 	{
 		/* Read Data Register */
-		if(USART_Word_Length_8BITS == g_USART_Config[1].USART_WordLength)
+		if(USART_Word_Length_8BITS == gp_USART_Config[1]->USART_WordLength)
 		{
-			if(USART_PARITY_BIT_NONE == g_USART_Config[1].USART_ParityBit)
+			if(USART_PARITY_BIT_NONE == gp_USART_Config[1]->USART_ParityBit)
 				*p_Buff = ( USARTx->DR & (uint8_t)0xFF );
 			else
 				*p_Buff = ( USARTx->DR & (uint8_t)0x7F );
 		}
-		else if(USART_Word_Length_9BITS == g_USART_Config[1].USART_WordLength)
+		else if(USART_Word_Length_9BITS == gp_USART_Config[1]->USART_WordLength)
 		{
-			if(USART_PARITY_BIT_NONE == g_USART_Config[1].USART_ParityBit)
+			if(USART_PARITY_BIT_NONE == gp_USART_Config[1]->USART_ParityBit)
 				*p_Buff = USARTx->DR;
 			else
 				*p_Buff = ( USARTx->DR & (uint8_t)0xFF );
@@ -278,16 +273,16 @@ void MCAL_USART_ReceiveData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Mec
 	else if(USART3 == USARTx)
 	{
 		/* Read Data Register */
-		if(USART_Word_Length_8BITS == g_USART_Config[2].USART_WordLength)
+		if(USART_Word_Length_8BITS == gp_USART_Config[2]->USART_WordLength)
 		{
-			if(USART_PARITY_BIT_NONE == g_USART_Config[2].USART_ParityBit)
+			if(USART_PARITY_BIT_NONE == gp_USART_Config[2]->USART_ParityBit)
 				*p_Buff = ( USARTx->DR & (uint8_t)0xFF );	/* All 8 Bits are Data */
 			else
 				*p_Buff = ( USARTx->DR & (uint8_t)0x7F ); 	/* 7 Bits are Data */
 		}
-		else if(USART_Word_Length_9BITS == g_USART_Config[2].USART_WordLength)
+		else if(USART_Word_Length_9BITS == gp_USART_Config[2]->USART_WordLength)
 		{
-			if(USART_PARITY_BIT_NONE == g_USART_Config[2].USART_ParityBit)
+			if(USART_PARITY_BIT_NONE == gp_USART_Config[2]->USART_ParityBit)
 				*p_Buff = USARTx->DR;	/* All Bits are Data */
 			else
 				*p_Buff = ( USARTx->DR & (uint8_t)0xFF );	/* 8 Bits are Data */
@@ -337,14 +332,14 @@ void MCAL_USART_GPIO_PinConfig(USART_TypeDef* USARTx)
 		MCAL_GPIO_Init(GPIOA, &config);
 
 		/* Configure Control Flow Pins */
-		if((USART_FLOW_CONTROL_CTS == g_USART_Config[0].USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == g_USART_Config[0].USART_FlowControl) )
+		if((USART_FLOW_CONTROL_CTS == gp_USART_Config[0]->USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == gp_USART_Config[0]->USART_FlowControl) )
 		{
 			/* GPIOA PIN11 CTS */
 			config.GPIO_PinNumber = GPIO_PIN11;
 			config.GPIO_Mode = GPIO_MODE_INPUT_AF_FLOATING;
 			MCAL_GPIO_Init(GPIOA, &config);
 
-		}else if((USART_FLOW_CONTROL_RTS == g_USART_Config[0].USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == g_USART_Config[0].USART_FlowControl))
+		}else if((USART_FLOW_CONTROL_RTS == gp_USART_Config[0]->USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == gp_USART_Config[0]->USART_FlowControl))
 		{
 			/* GPIOA PIN12 RTS */
 			config.GPIO_PinNumber = GPIO_PIN12;
@@ -368,14 +363,14 @@ void MCAL_USART_GPIO_PinConfig(USART_TypeDef* USARTx)
 		MCAL_GPIO_Init(GPIOA, &config);
 
 		/* Configure Control Flow Pins */
-		if((USART_FLOW_CONTROL_CTS == g_USART_Config[1].USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == g_USART_Config[1].USART_FlowControl) )
+		if((USART_FLOW_CONTROL_CTS == gp_USART_Config[1]->USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == gp_USART_Config[1]->USART_FlowControl) )
 		{
 			/* GPIOA PIN0 CTS */
 			config.GPIO_PinNumber = GPIO_PIN0;
 			config.GPIO_Mode = GPIO_MODE_INPUT_AF_FLOATING;
 			MCAL_GPIO_Init(GPIOA, &config);
 
-		}else if((USART_FLOW_CONTROL_RTS == g_USART_Config[1].USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == g_USART_Config[1].USART_FlowControl))
+		}else if((USART_FLOW_CONTROL_RTS == gp_USART_Config[1]->USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == gp_USART_Config[1]->USART_FlowControl))
 		{
 			/* GPIOA PIN1 RTS */
 			config.GPIO_PinNumber = GPIO_PIN1;
@@ -399,14 +394,14 @@ void MCAL_USART_GPIO_PinConfig(USART_TypeDef* USARTx)
 		MCAL_GPIO_Init(GPIOB, &config);
 
 		/* Configure Control Flow Pins */
-		if((USART_FLOW_CONTROL_CTS == g_USART_Config[2].USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == g_USART_Config[2].USART_FlowControl) )
+		if((USART_FLOW_CONTROL_CTS == gp_USART_Config[2]->USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == gp_USART_Config[2]->USART_FlowControl) )
 		{
 			/* GPIOB PIN13 CTS */
 			config.GPIO_PinNumber = GPIO_PIN13;
 			config.GPIO_Mode = GPIO_MODE_INPUT_AF_FLOATING;
 			MCAL_GPIO_Init(GPIOB, &config);
 
-		}else if((USART_FLOW_CONTROL_RTS == g_USART_Config[2].USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == g_USART_Config[2].USART_FlowControl))
+		}else if((USART_FLOW_CONTROL_RTS == gp_USART_Config[2]->USART_FlowControl) || (USART_FLOW_CONTROL_CTS_RTS == gp_USART_Config[2]->USART_FlowControl))
 		{
 			/* GPIOB PIN14 RTS */
 			config.GPIO_PinNumber = GPIO_PIN14;
@@ -425,20 +420,22 @@ void MCAL_USART_GPIO_PinConfig(USART_TypeDef* USARTx)
 void USART1_IRQHandler(void)
 {
 	/* Call The ISR CallBack Function */
-	if(gp_USART_ISR_CallBack[0] != NULL_PTR)
-		(*gp_USART_ISR_CallBack[0])();
+	if(gp_USART_Config[0]->p_USART_ISR != NULL)
+		(*gp_USART_Config[0]->p_USART_ISR)();
 }
 
 void USART2_IRQHandler(void)
 {
 	/* Call The ISR CallBack Function */
-	if(gp_USART_ISR_CallBack[1] != NULL_PTR)
-		(*gp_USART_ISR_CallBack[1])();
+	if(gp_USART_Config[1]->p_USART_ISR != NULL)
+		(*gp_USART_Config[1]->p_USART_ISR)();
 }
+
 
 void USART3_IRQHandler(void)
 {
 	/* Call The ISR CallBack Function */
-	if(gp_USART_ISR_CallBack[2] != NULL_PTR)
-		(*gp_USART_ISR_CallBack[2])();
+	if(gp_USART_Config[2]->p_USART_ISR != NULL)
+		(*gp_USART_Config[2]->p_USART_ISR)();
 }
+
--- STM32F103C8/Drivers/MCAL/USART/stm32f103c8_usart_driver.h
+++ STM32F103C8/Drivers/MCAL/USART/stm32f103c8_usart_driver.h
@@ -17,8 +17,6 @@
  *                                Includes                                       *
  ================================================================================*/
 #include "stm32f103c8.h"
-#include "stm32f103c8_gpio_driver.h"
-#include "stm32f103c8_rcc_driver.h"
 
 
 /*===============================================================================
@@ -32,7 +30,7 @@ typedef struct{
 
 	uint16_t USART_WordLength;		/* Specifies The USART WordLength Regarding to @ref WordLength_define*/
 
-	uint16_t USART_StopBits;			/* Specifies The USART StopBits Regarding to @ref StopBits_define */
+	uint16_t USART_StopBits;		/* Specifies The USART StopBits Regarding to @ref StopBits_define */
 
 	uint16_t USART_ParityBit;		/* Specifies The USART ParityBit Regarding to @ref ParityBit_define */
 
@@ -49,9 +47,9 @@ typedef struct{
  ================================================================================*/
 
 /* @ref USART_Mode_define */
-#define USART_MODE_TX						((uint32_t)0x00000008)
-#define USART_MODE_RX						((uint32_t)0x00000004)
-#define USART_MODE_TXRX						((uint32_t)0x0000000C)
+#define USART_MODE_TX						(0x00000008UL)
+#define USART_MODE_RX						(0x00000004UL)
+#define USART_MODE_TXRX						(0x0000000CUL)
 
 /* @ref BaudRate_Value_define */
 #define USART_BAUD_RATE_2400				2400
@@ -66,35 +64,35 @@ typedef struct{
 #define USART_BAUD_RATE_4500000				4500000
 
 /*  @ref WordLength_define */
-#define USART_Word_Length_8BITS				((uint32_t)0x00000000) 	/* 7 Data Bits and 1 Parity Bit */
-#define USART_Word_Length_9BITS				((uint32_t)0x00001000)	/* 8 Data Bits and 1 Parity Bit */
+#define USART_Word_Length_8BITS				(0x00000000UL) 	/* 7 Data Bits and 1 Parity Bit */
+#define USART_Word_Length_9BITS				(0x00001000UL)	/* 8 Data Bits and 1 Parity Bit */
 
 /* @ref StopBits_define */
-#define USART_STOP_BIT_1					((uint32_t)0x00000000)
-#define USART_STOP_BIT_2					((uint32_t)0x00002000)
+#define USART_STOP_BIT_1					(0x00000000UL)
+#define USART_STOP_BIT_2					(0x00002000UL)
 
 /* @ref ParityBit_define */
-#define USART_PARITY_BIT_NONE				((uint32_t)0x00000000)	/* Disable Parity Bit */
-#define USART_PARITY_BIT_EVEN				((uint32_t)0x00000400)	/* Even Parity Bit */
-#define USART_PARITY_BIT_ODD				((uint32_t)0x00000600)	/* Odd Parity Bit */
+#define USART_PARITY_BIT_NONE				(0x00000000UL)	/* Disable Parity Bit */
+#define USART_PARITY_BIT_EVEN				(0x00000400UL)	/* Even Parity Bit */
+#define USART_PARITY_BIT_ODD				(0x00000600UL)	/* Odd Parity Bit */
 
 /*  @ref FlowControl_define */
-#define USART_FLOW_CONTROL_NONE				((uint32_t)0x00000000)	/* Disable Flow Control */
-#define USART_FLOW_CONTROL_CTS				((uint32_t)0x00000200)
-#define USART_FLOW_CONTROL_RTS				((uint32_t)0x00000100)
-#define USART_FLOW_CONTROL_CTS_RTS			((uint32_t)0x00000300)
+#define USART_FLOW_CONTROL_NONE				(0x00000000UL)	/* Disable Flow Control */
+#define USART_FLOW_CONTROL_CTS				(0x00000200UL)
+#define USART_FLOW_CONTROL_RTS				(0x00000100UL)
+#define USART_FLOW_CONTROL_CTS_RTS			(0x00000300UL)
 
 /* @ref USART_Interrupt_Enable/Disable_define */
-#define USART_IRQ_DISABLE					((uint32_t)0x00000000)	/* Polling Mechanism Enabled */
-#define USART_IRQ_ENABLE_TXE				((uint32_t)0x00000080)	/* Transmit Data Register Empty */
-#define USART_IRQ_ENABLE_TC					((uint32_t)0x00000040)	/* Transmission Complete */
-#define USART_IRQ_ENABLE_RXNE				((uint32_t)0x00000020)	/* Received Data Ready to Be Read, Overrun Error detected */
-#define USART_IRQ_ENABLE_PE					((uint32_t)0x00000100)	/* Parity Error */
+#define USART_IRQ_DISABLE					(0x00000000UL)	/* Polling Mechanism Enabled */
+#define USART_IRQ_ENABLE_TXE				(0x00000080UL)	/* Transmit Data Register Empty */
+#define USART_IRQ_ENABLE_TC					(0x00000040UL)	/* Transmission Complete */
+#define USART_IRQ_ENABLE_RXNE				(0x00000020UL)	/* Received Data Ready to Be Read, Overrun Error detected */
+#define USART_IRQ_ENABLE_PE					(0x00000100UL)	/* Parity Error */
 
 typedef enum
 {
-	POLLING_DISABLED, POLLING_ENABLED
-}Polling_Mechanism;
+	USART_POLLING_DISABLED, USART_POLLING_ENABLED
+}USART_Polling_Mechanism;
 
 /*===============================================================================
  *           		    	   	   Generic Macros  		  	                     *
@@ -105,7 +103,7 @@ typedef enum
  ================================================================================*/
 
 /**===============================================================================
- * Function Name  : MCAL_GPIO_Init.
+ * Function Name  : MCAL_USART_Init.
  * Brief          : Function To Initiate a USART.
  * Parameter (in) : Instant USARTx Could be 1, 2, and 3.
  * Parameter (in) : Pointer to The Instant Configuration USART.
@@ -129,7 +127,7 @@ void MCAL_USART_Deinit(USART_TypeDef* USARTx);
  * Parameter (in) : Polling Mechanism State Enable/Disable.
  * Return         : None.
  * Note           : None.																					*/
-void MCAL_USART_TransmitData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Mechanism a_PollingEn);
+void MCAL_USART_TransmitData(USART_TypeDef* USARTx, uint16_t* p_Buff, USART_Polling_Mechanism a_PollingEn);
 
 /**===============================================================================
  * Function Name  : MCAL_USART_ReceiveData.
@@ -139,10 +137,10 @@ void MCAL_USART_TransmitData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Me
  * Parameter (in) : Polling Mechanism State Enable/Disable.
  * Return         : None.
  * Note           : None.																					*/
-void MCAL_USART_ReceiveData(USART_TypeDef* USARTx, uint16_t* p_Buff, Polling_Mechanism a_PollingEn);
+void MCAL_USART_ReceiveData(USART_TypeDef* USARTx, uint16_t* p_Buff, USART_Polling_Mechanism a_PollingEn);
 
 /**===============================================================================
- * Function Name  : MCAL_USART_ReceiveData.
+ * Function Name  : MCAL_USART_WaitTC.
  * Brief          : Function Waiting Transmission of the Data by USART is Completed.
  * Parameter (in) : Instant USARTx Could be 1, 2, and 3.
  * Return         : None.
